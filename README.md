e484fc2e6259f373d98ca5ccbed802617b3d4fba
blanpa <54003618+blanpa@users.noreply.github.com>
2023-07-09 11:21:14 +0200

    fix

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Machine_Learning_Demonstrator
 Machine_Learning_Demonstrator
+
+
+Streamlit App
+PyAudioAnalysis
+Keras Model
+
+Data from my own Model
--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 #%%
 import streamlit as st
-from pyAudioAnalysis import audioTrainTest as aT
+import librosa
 
 st.header("Machine Learning Demonstrator")
 
 #%%
-aT.extract_features_and_train(["./data/training/links","./data/training/rechts"], 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "svm", "svmSMtemp", False)
+
+st.spinner
+while True:
+    st.write(aT.extract_features_and_train(
+        ["./data/training/links","./data/training/rechts"], 
+        1.0, 1.0, 
+        aT.shortTermWindow, 
+        aT.shortTermStep, 
+        "svm", 
+        "svmSMtemp", 
+        False))
 
 #%%
 
 aT.file_classification("./data/test/test.wav", "svmSMtemp","svm")
+
 # %%
